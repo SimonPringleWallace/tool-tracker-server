eede8d04219afed1e8aa53b061c67dc8716ce537
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-08-31 09:39:58 -0400

    Setup up model tests
    - Add outline for model tests on models/tools_spec.rb

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Tool, type: :model do
-  skip "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a name'
+  it 'does not have a name containing special characters'
+  it 'is not valid without a quantity'
+  it 'is not valid without an availability status'
+  it "is not valid if 'available' is not a boolean"
 end
